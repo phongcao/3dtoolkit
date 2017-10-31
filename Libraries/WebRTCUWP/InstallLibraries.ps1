26cea77d4d8942b44663e9760d6b6b34f6d2ac79
Phong Cao <phong.caothai@live.com>
2017-10-31 11:43:52 -0400

    Updated DirectX HoloLens client to render the video frame with prediction.

--- Libraries/WebRTCUWP/InstallLibraries.ps1
+++ Libraries/WebRTCUWP/InstallLibraries.ps1
@@ -124,5 +124,5 @@ Test-Nano()
             ($EditionId -eq "ServerTuva"))
 }
 
-DecompressZip -filename "Org.WebRtc_m58_v1.0" -path "\Org.WebRTC\"
+DecompressZip -filename "Org.WebRtc_m58_timestamp" -path "\Org.WebRTC\"
 DecompressZip -filename "libyuv" -path "\libyuv\"
